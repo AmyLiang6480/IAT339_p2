470773ffd5a0ee432767506ee736682c9e77c8ab
Denys Ovsiienko <dovsiien@gmail.com>
2023-03-08 20:24:41 -0800

    Fixed bugs

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function addToCart(type) {
+  const name = type === 0 ? 'New Levels New Devils' : 'We Out Here'
+  alert(`'${name}' was added to your cart!`)
+}
+
+function submitReview() {
+  alert(`Your review was submitted!`)
+}
